8c71d304aa7210781487746a30bb88fdcb24da84
AyanC1 <choudhury.ayan13@gmail.com>
2019-03-16 21:37:41 +0530

    system_prop: Enable google assistant

--- system_prop.mk
+++ system_prop.mk
@@ -104,3 +104,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # SSR
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.ssr.restart_level=ALL_ENABLE
+
+#Google Assistant enabled here
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opa.eligible_device=true
